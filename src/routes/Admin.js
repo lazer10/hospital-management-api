f83bfe2f50b6c2d92e1b3943481532adb522456d
RAlain13 <mvumbalain@gmail.com>
2023-04-13 15:10:30 +0200

    final version

--- src/routes/Admin.js
+++ src/routes/Admin.js
@@ -3,6 +3,6 @@ import { Router } from 'express';
 import AdminController from '../controller/admin';
 
 const router = Router();
-router.post('/', AdminController.adminLogin);
+router.post('/login', AdminController.adminLogin);
 
 export default router;
