9daf5f4770c03616bd62fe2a7103a4c1cf7bd1b6
RAlain13 <mvumbalain@gmail.com>
2023-06-22 13:52:19 +0200

    change default doctor password V1

--- src/controller/doctor.js
+++ src/controller/doctor.js
@@ -100,23 +100,6 @@ class DoctorController {
       return out(res, 500, error.message || error, null, 'SERVER_ERROR');
     }
   }
-  /*
-  static async changeDefaultDoctorsPassword(req, res) {
-    try {
-      const { password: changedPassword } = req.body;
-      const { email } = req.user;
-
-      const hashedPassword = await generate(changedPassword);
-      const updatedPassword = await DoctorService.changeDoctorPassword(email, hashedPassword);
-
-      if (!updatedPassword) {
-        return out(res, 404, "Whoops! We can't find doctor with this email!", null, 'NOT_FOUND');
-      }
-      return out(res, 200, 'Password successfully changed!', updatedPassword);
-    } catch (error) {
-      return out(res, 500, error.message || error, null, 'SEVER_ERROR');
-    }
-  } */
 
   static async doctorChangeDefaultPassword(req, res) {
     try {
@@ -132,8 +115,8 @@ class DoctorController {
       if (newMatchesOld) return out(res, 400, 'Previous password must not match new password', null, 'AUTHENTICATION ERROR');
 
       const hashedPassword = await generate(newPassword);
-      const post = await DoctorService.changeDoctorPassword(hashedPassword, doctorExist.email);
-      console.log(post);
+      await DoctorService.changeDoctorPassword(hashedPassword, doctorExist.email);
+
       return out(res, 200, 'Password changed successfully');
     } catch (error) {
       return out(res, 500, error.message || error, null, 'SEVER_ERROR');
